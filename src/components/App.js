9e0f51b58911de4fa506bdfa5757fe7ab2906eb7
AZMAT ALI <222azmat@gmail.com>
2023-08-05 20:28:48 +0530

    submitting code-Sat Aug 05 2023 20:28:46 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,26 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
-
-
-
-
-import { useState } from 'react';
-
-const Modal = ({ show, onClose, children }) => {
-  if (!show) return null;
-
-  return (
-    <div>
-      <div>
-        <button className="modal-close" onClick={onClose}>
-          Close
-        </button>
-        <div className="modal-p">{children}</div>
-      </div>
-    </div>
-  );
-};
+import Modal from "./Modal";
 
 const App = () => {
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setModalShow] = useState(false);
 
-  const handleOpenModal = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
+  function handleCloseModal() {
+    setModalShow(false);
+  }
 
   return (
-    <div className="modal-overlay">
-      <h1>Modal Example</h1>
-      <button onClick={handleOpenModal}>Open Modal</button>
-      <Modal show={showModal} onClose={handleCloseModal}>
-        <p className="modal-p">This is the content inside the modal.</p>
-      </Modal>
+    <div id="main">
+       <button onClick={()=>setModalShow(true)}>Open Modal</button>
+       <Modal show={showModal} onClose={handleCloseModal}>
+          <div className="modal">
+            <h2>Modal Content</h2>
+            <p>This is the content of the modal.</p>
+          </div>
+       </Modal>
     </div>
-  );
-};
+  )
+}
 
-export default App;
-
+export default App
